8ef2375fd67faabbea7448e28ea1d2ad5eac724c
WalterJC <jc.walter@hotmail.fr>
2026-03-12 13:27:40 +0100

    feat: auto-create superuser on deploy

--- build.sh
+++ build.sh
@@ -12,3 +12,14 @@ python manage.py collectstatic --no-input
 
 # Applique les migrations de base de données
 python manage.py migrate
+
+# Crée le superuser admin si il n'existe pas déjà
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@healthnorth.com', 'admin1234')
+    print('Superuser créé')
+else:
+    print('Superuser existe déjà')
+"
